e674f6359df1620a70498685aa3c69dd6544a714
izafurieridelpuppo <82715832+izafurieridelpuppo@users.noreply.github.com>
2021-11-18 21:52:55 -0300

    Create Andar.cs

---
+++ Andar.cs
@@ -0,0 +1,11 @@
+namespace Elevador
+{
+    public class Andar
+    {
+        // Identificação do Andar = 0,1,2,3 e 4
+        public int Id { get; set; }
+
+        // Identicação do Acionamento do botão do andar: 0 = não e 1 = sim 
+        public bool Click { get; set; }
+    }
+}
